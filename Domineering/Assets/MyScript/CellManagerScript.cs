0a00f0f68971a156248f33541e2b64c978bca315
LeRyokan <tixier.lucas@gmail.com>
2016-03-03 08:10:23 +0100

    Création Projet sous unity.

---
+++ Domineering/Assets/MyScript/CellManagerScript.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CellManagerScript : MonoBehaviour {
+/*
+    [SerializeField]
+    int posX;
+
+    [SerializeField]
+    int posY;
+*/
+    [SerializeField]
+    BoardManagerScript boardManagerScript;
+
+    Material Mat;
+
+    bool isClicked;
+
+	// Use this for initialization
+	void Start () {
+        Mat = GetComponent<Renderer>().material;
+        isClicked = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnMouseOver()
+    {
+        if (!isClicked)
+        {
+            Debug.Log("On est au dessus du lot");
+            Mat.color = Color.magenta;
+        }
+    }
+
+    void OnMouseExit()
+    {
+        if (!isClicked)
+        {
+            Debug.Log("On SORT");
+            Mat.color = Color.white;
+        }
+    }
+
+    void OnMouseDown()
+    {
+        isClicked = true;
+        Mat.color = Color.cyan;
+        boardManagerScript.YOLO();
+    }
+}
