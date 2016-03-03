4bf1b66e07f975b6529b92e10747ba1e15ae1d3f
LeRyokan <tixier.lucas@gmail.com>
2016-03-03 10:16:55 +0100

    Commit de la scene

--- Domineering/Assets/MyScript/CellManagerScript.cs
+++ Domineering/Assets/MyScript/CellManagerScript.cs
@@ -1,14 +1,15 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class CellManagerScript : MonoBehaviour {
-/*
-    [SerializeField]
-    int posX;
+public class CellManagerScript : MonoBehaviour
+{
+    /*
+        [SerializeField]
+        int posX;
 
-    [SerializeField]
-    int posY;
-*/
+        [SerializeField]
+        int posY;
+    */
     [SerializeField]
     BoardManagerScript boardManagerScript;
 
@@ -21,50 +22,35 @@ public class CellManagerScript : MonoBehaviour {
 
     public bool isClicked;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
         Mat = GetComponent<Renderer>().material;
         isClicked = false;
         posX = (int)(transform.position.x + 3.5);
         posY = (int)(transform.position.y + 3.5);
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
 
     void OnMouseOver()
     {
-        if (!isClicked)
-        {
-            Debug.Log("On est au dessus du lot");
-          //  Mat.color = Color.magenta;
-            boardManagerScript.ShowCellsToColor(posX, posY);
-        }
+        boardManagerScript.ShowCellsToColor(posX, posY);
     }
 
     void OnMouseExit()
     {
-        if (!isClicked)
-        {
-            Debug.Log("On SORT");
-           // Mat.color = Color.white;
-            boardManagerScript.VoidColorOnMouseExit(posX, posY);
-        }
+        boardManagerScript.VoidColorOnMouseExit(posX, posY);
     }
 
     void OnMouseDown()
     {
-        if (!isClicked)
-        {
-            isClicked = true;
-           // Mat.color = Color.cyan;
-            boardManagerScript.ColorCells(posX, posY);
-        }
-       // boardManagerScript.getCellFromPos(posX,posY);
-
+        boardManagerScript.ColorCells(posX, posY);
     }
 
-    
+
 }
