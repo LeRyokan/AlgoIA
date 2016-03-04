39f4f727b9313aa4c3bb89b98bfe8e3519a9c611
makioto <makioto@voila.fr>
2016-03-04 08:50:20 +0100

    Attente entre deux actions d'IA + booléen dans IA pour history ou non

--- Domineering/Assets/MyScript/IA.cs
+++ Domineering/Assets/MyScript/IA.cs
@@ -13,15 +13,31 @@ public class IA : MonoBehaviour
 	[SerializeField]
 	public bool h_or_v = true;
 
+	[SerializeField]
+	public bool History = false;
+
 	// Use this for initialization
 	void Start () 
 	{
 	
 	}
+
+	public static int frame_wait = 0;
 	
 	// Update is called once per frame
 	void Update () 
 	{
+		// attends 10 frames entre deux actions d'ia
+		if (frame_wait < 10) 
+		{
+			if (board.Player1 == h_or_v) 
+			{
+				frame_wait++;
+			}
+
+			return;
+		}
+
 		if (board == null || !board.canPlay(h_or_v ? 1 : 0) ) 
 		{
 			return;
@@ -29,9 +45,10 @@ public class IA : MonoBehaviour
 
 		if (board.Player1 == h_or_v) 
 		{
-			Move move = board.NegaMove(h_or_v ? 1 : 0, profondeur);
+			Move move = History ? board.NegaMoveHistory(h_or_v ? 1 : 0, profondeur) : board.NegaMove(h_or_v ? 1 : 0, profondeur);
 			board.DoMove(move);
 			board.Player1 = !board.Player1;
+			frame_wait = 0;
 		}
 
 	}
